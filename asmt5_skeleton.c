e05cf17a574990f6f9cd241f4caff7643b39a957
Maxwell Plotkin <plotkinmaxwell@gmail.com>
2018-05-15 16:27:07 -0700

    asmt5 base functionality

---
+++ asmt5_skeleton.c
@@ -0,0 +1,113 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+/* Define structure for instruction containing fields for destination register, 2 source registers, and individual instruction delay 
+and a variable as pointer to structure for creating a dynamic array of instructions */
+
+
+/*************************************************************/
+void FUNCTION TO PRINT OUT CHART OF INSTRUCTIONS WITH DELAYS() 
+{
+/* Declare local variables */
+/* For each instruction, align instructions by tabbing according to delay, and print out stages with proper tabbing (FI\tDI\tCO\tFO\tEI\tWO) */
+  return;
+}
+
+
+/*************************************************************/
+void FUNCTION TO ENTER INSTRUCTIONS()
+{
+/* Declare local variables, including an array of characters to store user input */
+
+/* Prompt for total number of instructions */
+ 
+/* Allocate memory to hold a set of instructions based on total number of instructions+1 (instruction 0 used for dependency checking)*/
+
+/* Initialize instruction 0's destination register to -1 to prevent false RAW dependency w/ instruction 2 */
+
+/* For each instruction, prompt for user input with instruction number, such as: 1)   
+and read instruction as a string and store at proper field of appropriate index within dynamic array of instructions */
+
+return;
+}
+
+
+/*****************************************************/
+void FUNCTION TO CALCULATE DELAY OF SET OF INSTRUCTIONS ON A 6-STAGE PIPLELINE ARCHITECTURE
+{
+/* Declare local variables */
+
+/* For each instruction i from 2 to total number of instructions, initialize delay as 0 and check for dependency
+between instruction (i-2) and i, as well as between instruction (i-1) and i */
+
+{ /* begin for-loop */
+
+/* If dependency,  set delay appropriately, as well
+as a flag to check for possible overlap between dependent instructions */
+
+/* Note: general formula for delay: 
+delay=2 if instruction i depends on instruction (i-1)
+delay=1 if instruction i depends on instruction (i-2) and no overlap of dependencies
+delay=0 otherwise
+*/
+
+/* Calculate individual delay for current instruction */
+
+} /* end for-loop */
+
+/* print chart */
+return;
+}
+
+/***************************************************************/
+
+void FUNCTION TO CALCULATE DELAY OF SET OF INSTRUCTIONS ON A 6-STAGE SUPERSCALAR ARCHITECTURE
+{
+/* Declare local variables */
+
+/* For each instruction i from 2 to total number of instructions */
+
+{ /* begin for-loop */
+
+/* Flip the flag used for expected/non-expected pipeline delay to resemble delay pattern for superscalar: 
+ if flag is 0, set it to 1, if it is 1, reset it to 0 */
+
+initialize delay as 0 and check for dependencies
+between instruction (i-4) and i, (i-3) and i, all the way to (i-1) and i */
+
+/* If dependency, set delay appropriately, as well as setting flag for expected pipeline delay to 1, and set
+flag to check for possible overlap between dependent instructions */
+
+/* Note: general formula for delay: 
+delay=2 if instruction i depends on instruction (i-1)
+
+delay=2 if instruction i depends on instruction (i-2) and expected pipeline delay and no overlap of dependencies
+
+delay=1 if instruction i depends on instruction (i-2) and no expected pipeline delay and no overlap of dependencies 
+
+delay=1 if instruction i depends on instruction (i-3) and no overlap of dependencies
+
+delay=1 if instruction i depends on instruction (i-4) and expected pipeline delay and no overlap of dependencies 
+
+delay=0 otherwise
+*/
+
+/* Calculate individual delay for current instruction */
+
+} /* end for-loop */
+
+/* print chart */
+
+return;
+}
+
+
+/****************************************************************/
+int main()
+{
+/* Declare local variables */
+
+/* Until user quits, print menu, prompt for user input, and select corresponding function */
+
+return 1;
+}
---
+++ assignment5.c
@@ -0,0 +1,98 @@
+/*
+ * Maxwell Plotkin
+ * COMP222 Spring 2018
+ * Assignment 5
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Inst {
+	int dst;
+	int src1;
+	int src2;
+	int delay;
+} Inst;
+
+int main(void) {
+	Inst* sb; //scoreboard
+	char s; //selection
+	char in[8]; //input string
+	int ic = -1; //instruction count
+	int d = 0; //delay
+	int i, j; //iterators
+
+	while(1) {
+		printf(
+			"\n\n"
+			"Instruction-Level Parallelism\n"
+			"------------------------------\n"
+			"a) Enter Instructions\n"
+			"b) Calculate Cycle Count, 6-Stage Pipeline\n"
+			"c) Quit Program\n"
+			"\n"
+			"Enter Selection: "
+		);
+		s = getchar(); getchar(); //flush buffer
+		printf("\n");
+
+		switch(s) {
+			case 'a':
+			case 'A':
+				do {
+					printf("Enter Instruction Count: ");
+					scanf("%d", &ic); getchar();  //flush buffer
+
+					if(ic <= 0)
+						printf("Invalid Size, please choose a positive integer.\n");
+				} while(ic <= 0);
+
+				sb = malloc((ic+1) * sizeof(Inst));
+				sb[0] = (Inst) {-1, -1, -1, -1};
+
+				for(i = 1; i < ic+1; ++i) {
+					printf("%d) ", i); 
+					scanf("%s", in); getchar();
+					sb[i] = (Inst) {in[1]-'0', in[4]-'0', in[7]-'0', 0};
+				}
+
+				break;
+			case 'b':
+			case 'B':
+				if(ic <= 0) {
+					printf("Please enter instructions.");
+				} else {
+					for(i = 1; i < ic+1; ++i) {
+						if(i != 1) {
+							if((sb[i].src1 == sb[i-1].dst) || (sb[i].src2 == sb[i-1].dst)) {
+								d += 2;
+								for(j = i; j < ic+1; ++j)
+									sb[j].delay += 2;
+							} else if((sb[i].src1 == sb[i-2].dst) || (sb[i].src2 == sb[i-2].dst)) {
+								d += 1;
+								for(j = i; j < ic+1; ++j)
+									sb[j].delay += 1;
+							}
+						}
+
+						printf("%d)", i);
+
+						for(j = 1 - i; j < sb[i].delay; ++j) {
+							printf("\t");
+						}
+
+						printf("FI\tDI\tCO\tFO\tEI\tWO\n");
+					}
+
+					printf("\nTotal Cycles: %d", ic + 5 + d);
+				}
+				break;
+			case 'c':
+			case 'C':
+				printf("Exiting\n");
+				return 0;
+			default:
+				printf("Invalid Selection\n");
+				break;
+		}
+	}
+}
